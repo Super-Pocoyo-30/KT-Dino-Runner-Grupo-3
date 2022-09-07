b140e7de7ba7a83e1bdd7042799e2abd65575a23
Kevin-Torrejon-30 <mirkokevintorrejon@gmail.com>
2022-09-06 22:17:20 -0400

    Tarea 2: LARGE_CACTUS
    La tarea de la clase 2

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -3,8 +3,7 @@ import random
 from dino_runner.components.obstacles.obstacle import Obstacle
 
 class Cactus(Obstacle):
-    def __init__(self, image):
-        self.type = random.randint(0, 2)
-        super().__init__(image, self.type)
+    def __init__(self, image, image2):
+        self.type = random.randint(0, 5)
+        super().__init__(image, image2, self.type)
         self.rect.y = 325
-        
--- dino_runner/components/obstacles/obstacle.py
+++ dino_runner/components/obstacles/obstacle.py
@@ -6,17 +6,22 @@ from dino_runner.utils.constants import SCREEN_WIDTH
 
 
 class Obstacle(Sprite):
-    def __init__(self, image, obstacle_type):
-        self.image = image
+    def __init__(self, image, image2, obstacle_type):
+        self.image = image + image2
         self.obstacle_type = obstacle_type
         self.rect = self.image[self.obstacle_type].get_rect()
         self.rect.x = SCREEN_WIDTH
-
 
     def update(self, game_speed, obstacles):
         self.rect.x -= game_speed
         if self.rect.x < -self.rect.width:
             obstacles.pop()
+
+        if self.obstacle_type in [3,4,5]:
+            self.rect.y = 300
+        else:
+            self.rect.y = 320
+
 
     def draw(self, screen):
         screen.blit(self.image[self.obstacle_type],(self.rect.x, self.rect.y))

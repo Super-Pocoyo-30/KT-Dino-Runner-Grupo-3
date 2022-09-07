82eb9816cde4545a48683ebc3705f9259a138f12
Kevin-Torrejon-30 <mirkokevintorrejon@gmail.com>
2022-09-06 21:06:48 -0400

    Clase 2 : Obstacles
    en este commit se encuentra lo avanzado en la clase 1 y algunas correcciones de la tarea (Metodo duck)

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,10 +1,10 @@
 import pygame
-
+from pygame.sprite import Sprite
 
 from dino_runner.utils.constants import DUCKING, JUMPING, RUNNING
 
 
-class Dinosaur:
+class Dinosaur(Sprite):
     X_POS = 80
     Y_POS = 310
     JUMP_VEL = 8.5
@@ -32,15 +32,11 @@ class Dinosaur:
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
-        elif not self.dino_jump:
-            self.dino_jump = False
-            self.dino_run = True
-
-        if user_input[pygame.K_DOWN] :
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
             self.dino_duck = True
             self.dino_run = False
-        elif not self.dino_duck:
-            self.dino_duck = False
+        elif not self.dino_jump:
+            self.dino_jump = False
             self.dino_run = True
 
         if self.step_index >= 10:
@@ -67,7 +63,7 @@ class Dinosaur:
         self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.X_POS
-        self.dino_rect.y = self.Y_POS 
+        self.dino_rect.y = 350 
         self.step_index += 1
 
     def draw(self, screen):

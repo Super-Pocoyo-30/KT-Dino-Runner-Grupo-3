5bda601786519d7177d289d01004e202f2ca0db9
Kevin-Torrejon-30 <mirkokevintorrejon@gmail.com>
2022-09-07 23:45:16 -0400

    Clase 3: Menu
    este commit contiene lo avanzado en la clase 3 mas las correcciones de la tarea LARGE_CACTUS

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,25 +1,30 @@
 import pygame
-
+import random
 
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS,LARGE_CACTUS
+from dino_runner.utils.constants import LARGE_CACTUS, SMALL_CACTUS
 
 class ObstacleManager:
     def __init__(self):
-        self.obstacles = []
+        self.obstacles = [] 
     
     def update(self, game):
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS, LARGE_CACTUS))
+            cactus_type = "SMALL" if random.randint(0,1) == 0 else "LARGE"
+            self.obstacles.append(Cactus(cactus_type))
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
                 game.playing = False
+                game.death_count += 1
                 break
         
 
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
+
+    def reset_obstacles(self):
+        self.obstacles = []
